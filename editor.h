e2b0939e0a4a467759ad459d4e8853556c23c08f
SG master <1363028533@qq.com>
2018-03-05 23:57:40 +0800

    Update.

--- editor.h
+++ editor.h
@@ -1,15 +1,22 @@
 #pragma once
 #include "file.h"
 
+#define EDITOR_WIDTH 640
+#define EDITOR_HEIGHT 480
+
 class Editor {
 private:
 	File *content;
+	int fileNum = 0;
+
+	int active = -1;
 
 public:
 	Editor();
 	~Editor();
 
 	void open(char *filename);
+	void refresh();
 	void close();
 	void save();
 
