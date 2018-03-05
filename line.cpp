bd5da34f53dc20d283084ec2f78bc69697220107
SG master <1363028533@qq.com>
2018-03-05 23:57:50 +0800

    Update.

--- line.cpp
+++ line.cpp
@@ -1,1 +1,13 @@
 #include "line.h"
+
+Line::Line(vector<string> &cont, int &count, int offset) : content(NULL), next(NULL) {
+	count++;
+
+	content = new string(cont[offset]);
+	if (offset + 1 < cont.size())
+		next = new Line(cont, count, offset + 1);
+}
+Line::~Line() {
+	if (next)delete next;
+	delete content;
+}
