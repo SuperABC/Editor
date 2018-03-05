051929cfdfb29e14598bf3be6f3cf220da1e4b22
SG master <1363028533@qq.com>
2018-03-05 23:57:32 +0800

    Update.

--- block.cpp
+++ block.cpp
@@ -1,1 +1,21 @@
 #include "block.h"
+
+Block::Block(vector<string> &cont, int &count, int offset) : content(NULL), next(NULL) {
+	int start = offset, end = offset;
+	int length = 0;
+
+	count++;
+	while (length < BLOCK_SIZE / 2 && end < cont.size()) {
+		length += cont[end++].length();
+	}
+	vector<string> line(cont.begin() + start, cont.begin() + end);
+	content = new Line(line, totalLine);
+	if(end < cont.size())
+		next = new Block(cont, count, end);
+
+	startLine = 0;
+};
+Block::~Block() {
+	if (next)delete next;
+	delete content;
+}
