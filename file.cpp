2eb6492c6fbcb522f2d19540e2b26cecd563598f
SG master <1363028533@qq.com>
2018-03-05 23:57:43 +0800

    Update.

--- file.cpp
+++ file.cpp
@@ -1,1 +1,20 @@
 #include "file.h"
+
+File::File(char *filename) : filename(filename), content(NULL), next(NULL) {
+	string tmp;
+	vector<string> whole;
+
+	std::ifstream fin(filename);
+
+	while (!fin.eof()) {
+		getline(fin, tmp);
+		whole.push_back(tmp);
+	}
+
+	content = new Block(whole, totalBlock);
+	startBlock = 0;
+	fin.close();
+};
+File::~File() {
+	delete content;
+}
