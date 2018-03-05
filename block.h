13f1b02d3b11a542cb6a4a1b36f4167fcfdcfddf
SG master <1363028533@qq.com>
2018-03-05 23:57:35 +0800

    Update.

--- block.h
+++ block.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "line.h"
 
+#define BLOCK_SIZE 4096
+
 class Block {
 private:
 	Line *content;
 	Block *next;
 
+	int startLine = -1;
+	int totalLine = 0;
+
 	friend class File;
+	friend class Editor;
 public:
-	Block() :content(NULL), next(NULL) {};
+	Block(vector<string> &cont, int &count, int offset = 0);
+	~Block();
 };
