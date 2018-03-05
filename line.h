188eca830398a3884f564a31b26bb264182b6380
SG master <1363028533@qq.com>
2018-03-05 23:57:53 +0800

    Update.

--- line.h
+++ line.h
@@ -1,15 +1,25 @@
 #pragma once
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 #include "winsgl.h"
+
+using std::string;
+using std::vector;
 
 class Line {
 private:
-	char content;
+	string *content;
 	Line *next;
 
 	friend class Block;
+	friend class File;
+	friend class Editor;
 public:
-	Line(char c) :content(c), next(NULL) {}
+	Line(string *c) :content(c), next(NULL) {}
+	Line(vector<string> &cont, int &count, int offset = 0);
+	~Line();
 	
 	char get();
 
