e60d7345aa89e2606a70fe2c55199d3d84bb478f
SG master <1363028533@qq.com>
2018-03-15 14:26:35 +0800

    Update.

--- block.h
+++ block.h
@@ -6,14 +6,21 @@
 class Block {
 private:
 	Line *content;
-	Block *next;
+	Block *next, *prev;
 
-	int startLine = -1;
-	int totalLine = 0;
+	int start = 0;
+	int total = 0;
+	int focus = -1;
+
+	int size = 0;
 
 	friend class File;
 	friend class Editor;
 public:
+	Block(string str = string());
 	Block(vector<string> &cont, int &count, int offset = 0);
 	~Block();
+
+	Line *startLine();
+	Line *focusLine();
 };
