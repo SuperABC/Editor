b25c976323ab4a7358187b0c1ae0a0b2d3acbc42
SG master <1363028533@qq.com>
2018-03-15 14:26:50 +0800

    Update.

--- file.h
+++ file.h
@@ -3,16 +3,21 @@
 
 class File {
 private:
-	char *filename;
+	string filename;
 
 	Block *content;
-	File *next;
+	File *next, *prev;
 
-	int startBlock = -1;
-	int totalBlock = 0;
+	int start = 0;
+	int total = 0;
+	int focus = -1;
 
 	friend class Editor;
 public:
+	File();
 	File(char *filename);
 	~File();
+
+	Block *startBlock();
+	Block *focusBlock();
 };
