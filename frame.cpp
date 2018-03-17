3cf8c9bd09b5b09664f8ef376871642f675fa452
SG master <1363028533@qq.com>
2018-03-17 23:46:32 +0800

    Update.

--- frame.cpp
+++ frame.cpp
@@ -1,13 +1,20 @@
-#include "editor.h"
+#include "editor/editor.h"
+#include "sgs/machine.h"
 
 Editor *editor;
 
+int ctrl = 0;
+
+/* Menu call. */
 void callBack() {
 
 }
 
 void newFile() {
-	editor->create();
+	char dir[256] = { 0 };
+	selectDir(dir, NULL);
+	if (dir[0] == 0)return;
+	editor->create(dir);
 	editor->refresh();
 }
 void openFile() {
@@ -23,11 +30,24 @@ void openFile() {
 void saveFile() {
 	editor->save();
 }
+void resaveFile() {
+	char *src = new char[256];
+	memset(src, 0, 256);
+
+	selectSave(src, NULL, (char *)"sgs");
+	if (src[0] == '\0')return;
+
+	editor->save(src);
+	editor->refresh();
+}
 void closeFile() {
 	editor->close();
 }
 void exitEditor() {
 	exit(0);
+}
+void interpRun() {
+	createThread(interprete);
 }
 void versionInfo() {
 	alertInfo("现在还没有完成第一版哟~再等等吧", "版本",
@@ -41,7 +61,7 @@ void companyInfo() {
 	alertInfo("浙江大学玉泉校区32舍420", "公司",
 		ALERT_ICON_INFORMATION | ALERT_BUTTON_OK);
 }
-void editroInst() {
+void editorInst() {
 	alertInfo("适用SGL、SGS、RWE等项目的编辑器，没什么新特性", "编辑器简介",
 		ALERT_ICON_INFORMATION | ALERT_BUTTON_OK);
 }
@@ -50,20 +70,42 @@ void userInst() {
 		ALERT_ICON_INFORMATION | ALERT_BUTTON_OK);
 }
 
-void sgSetup() {
-	initWindow(640, 480, "Editor", BIT_MAP);
-	initKey();
-	initMouse(SG_COORDINATE);
+/* Widget call. */
+void windowResize(int x, int y) {
+	editor->resize(x, y);
+	editor->refresh();
+}
+void logClearCall(widgetObj *w, int x, int y, int status) {
+	mouseClickDefault(w, x, y, status);
+	if(w->status & WIDGET_SELECTED)editor->logClear();
+}
+void logCopyCall(widgetObj *w, int x, int y, int status) {
+	mouseClickDefault(w, x, y, status);
+	if (w->status & WIDGET_SELECTED)copyText((char *)getWidgetByName("log")->content);
+}
 
+/* Initialize func. */
+void layoutMenu() {
 	initMenu();
+
 	int idFile = addMenuList("文件", 0);
 	int idEdit = addMenuList("编辑", 0);
+	int idComp = addMenuList("编译器", 0);
+	int idTool = addMenuList("工具", 0);
 	int idHelp = addMenuList("帮助", 0);
+
 	addMenuItem("新建\tCtrl + N", idFile, newFile);
 	addMenuItem("打开\tCtrl + O", idFile, openFile);
+	int idDir = addMenuList("打开文件夹", idFile);
+	addMenuItem("资源管理器", idDir, callBack);
+	addMenuItem("命令行", idDir, callBack);
 	addMenuItem("保存\tCtrl + S", idFile, saveFile);
+	addMenuItem("另存为", idFile, resaveFile);
 	addMenuItem("关闭", idFile, closeFile);
+	addMenuItem("全部关闭", idFile, callBack);
+	addMenuSeparator(idFile);
 	addMenuItem("退出\tF4", idFile, exitEditor);
+
 	addMenuItem("撤销\tCtrl + Z", idEdit, callBack);
 	addMenuItem("重做\tCtrl + Y", idEdit, callBack);
 	addMenuSeparator(idEdit);
@@ -71,15 +113,42 @@ void sgSetup() {
 	addMenuItem("剪切\tCtrl + X", idEdit, callBack);
 	addMenuItem("粘贴\tCtrl + V", idEdit, callBack);
 	addMenuItem("清除", idEdit, callBack);
+	addMenuSeparator(idEdit);
+	addMenuItem("添加/删除注释", idEdit, callBack);
+	addMenuItem("大小写转换", idEdit, callBack);
+
+	addMenuItem("解释运行", idComp, interpRun);
+	addMenuItem("解释调试", idComp, callBack);
+	addMenuSeparator(idComp);
+	addMenuItem("编译器设置", idComp, callBack);
+
+	int idOption = addMenuList("选项", idTool);
+	addMenuItem("显示设置", idOption, callBack);
+	addMenuItem("启动设置", idOption, callBack);
+	addMenuItem("操作设置", idOption, callBack);
+	int idPlugin = addMenuList("插件", idTool);
+	addMenuItem("管理器", idPlugin, callBack);
+
 	int idAbout = addMenuList("关于", idHelp);
 	addMenuItem("版本", idAbout, versionInfo);
 	addMenuItem("作者", idAbout, authorInfo);
 	addMenuItem("公司", idAbout, companyInfo);
 	int idInst = addMenuList("说明", idHelp);
-	addMenuItem("编辑器简介", idInst, editroInst);
+	addMenuItem("编辑器简介", idInst, editorInst);
 	addMenuItem("使用方法", idInst, userInst);
 
-	editor = new Editor();
+}
+
+/* Main frame. */
+void sgSetup() {
+	initWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Editor", BIT_MAP);
+	setResizeable();
+	resizeFuntion(windowResize);
+	initKey();
+	initMouse(SG_COORDINATE);
+
+	editor = new Editor("config.ini");
+	layoutMenu();
 }
 void sgLoop() {
 	int key;
@@ -89,13 +158,29 @@ void sgLoop() {
 	static int first = 1;
 	if (first) {
 		first = 0;
-		setColor(64, 64, 64);
-		clearScreen();
+		editor->refresh();
 	}
 
 	if (biosKey(1)) {
 		key = biosKey(0);
-		editor->key(key);
+
+		if (key == SG_CTRL) {
+			ctrl = 1;
+			return;
+		}
+		else if (key == (SG_CTRL | 0X8000)) {
+			ctrl = 0;
+			return;
+		}
+
+		if (key == SG_F4)exitEditor();
+
+		if (ctrl) {
+
+		}
+		else {
+			editor->key(key);
+		}
 	}
 	if (biosMouse(1).m) {
 		mouse = biosMouse(0);
@@ -103,16 +188,19 @@ void sgLoop() {
 	}
 
 	if ((clock() - time) % 800 < 400) {
-		setColor(255, 255, 255);
-		putQuad(editor->cursorPos.x - 1, editor->cursorPos.y + 1,
-			editor->cursorPos.x, editor->cursorPos.y + editor->fontSize + 4 - 2, SOLID_FILL);
+		setColor(editor->cursorColor.r, editor->cursorColor.g, editor->cursorColor.b);
+		putQuad(editor->editorBase.x + editor->cursorPos.x - 1,
+			editor->editorBase.y + editor->cursorPos.y + 1,
+			editor->editorBase.x + editor->cursorPos.x,
+			editor->editorBase.y + editor->cursorPos.y + editor->fontSize + 4 - 4,
+			SOLID_FILL);
 	}
 	else {
 		setColor(64, 64, 64);
-		putQuad(editor->cursorPos.x - 1, editor->cursorPos.y + 1,
-			editor->cursorPos.x, editor->cursorPos.y + editor->fontSize + 4 - 2, SOLID_FILL);
+		putQuad(editor->editorBase.x + editor->cursorPos.x - 1,
+			editor->editorBase.y + editor->cursorPos.y + 1,
+			editor->editorBase.x + editor->cursorPos.x,
+			editor->editorBase.y + editor->cursorPos.y + editor->fontSize + 4 - 4,
+			SOLID_FILL);
 	}
-
-	clearKeyBuffer();
-	clearMouseBuffer();
 }
