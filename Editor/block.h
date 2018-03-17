75f95463b29405762bd105b79b5ffb1b2abbdc34
SG master <1363028533@qq.com>
2018-03-17 23:46:12 +0800

    Update.

---
+++ Editor/block.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "line.h"
+
+#define BLOCK_SIZE 4096
+
+class Block {
+private:
+	Line *content;
+	Block *next, *prev;
+
+	int start = 0;
+	int total = 0;
+	int focus = -1;
+
+	int size = 0;
+
+	friend class File;
+	friend class Editor;
+public:
+	Block(string str = string());
+	Block(vector<string> &cont, int &count, int offset = 0);
+	~Block();
+
+	Line *startLine();
+	Line *focusLine();
+};
