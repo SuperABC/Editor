715acbae9f3968bd5595417b867218c092484a41
SG master <1363028533@qq.com>
2018-03-25 20:58:12 +0800

    Update.

--- Editor/block.cpp
+++ Editor/block.cpp
@@ -1,18 +1,24 @@
 #include "block.h"
 
-Block::Block(string str) : next(NULL), prev(NULL) {
+Block::Block(string str): next(NULL), prev(NULL) {
 	content = new Line(str);
 	size = 0;
 	start = 0;
 	total = 1;
 	focus = 0;
+	selectBegin = 0;
+	selectEnd = 0;
 }
 Block::Block(vector<string> &cont, int &count, int offset) :
 	content(NULL), next(NULL), prev(NULL) {
 	int start = offset, end = offset;
 	int length = 0;
 
-	if (count == 0)focus = 0;
+	if (count == 0) {
+		focus = 0;
+		selectBegin = 0;
+		selectEnd = 0;
+	}
 	count++;
 
 	while (length < BLOCK_SIZE / 2 && end < (int)cont.size()) {
@@ -43,5 +49,10 @@ Line *Block::startLine() {
 Line *Block::focusLine() {
 	Line *tmpLine = content;
 	for (int i = 0; i < focus; i++)tmpLine = tmpLine->next;
+	return tmpLine;
+}
+Line *Block::lineAt(int n){
+	Line *tmpLine = content;
+	for (int i = 0; i < n; i++)tmpLine = tmpLine->next;
 	return tmpLine;
 }
